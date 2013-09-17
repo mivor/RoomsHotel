8c8a4f70166f29cb182625ec8b59ae8cdbf4949a
Ferenc Grecu <mivor20@gmail.com>
2013-09-17 19:54:19 +0300

    Check if hotel program is running

--- RoomsHotel/Program.cs
+++ RoomsHotel/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Odbc;
+using System.Diagnostics;
 
 namespace RoomsHotel
 {
@@ -15,13 +16,25 @@ namespace RoomsHotel
         [STAThread]
         static void Main()
         {
+
+            bool isRunning;
+
+            Process app = Process.GetProcessesByName("Hotel").FirstOrDefault();
+
+            isRunning = app != default(Process);
+
+            string connectionString = @"Driver={{Microsoft Paradox Driver (*.db )}};DriverID=538;
+                   Fil=Paradox 7.X;DefaultDir=C:\\DB;Dbq=C:\\DB;
+                   CollatingSequence=ASCII;";
+
+            OdbcConnection DbConnection = new OdbcConnection(connectionString);
+
+            // Visual part
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            string connectionString = "";
 
-            OdbcConnection DbConnection = new OdbcConnection(connectionString);
         }
     }
 }
